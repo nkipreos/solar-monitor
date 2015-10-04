8be2395572d08ff196c3f10f94cdbb5c63d1fda1
nkipreos <nkipreos@gmail.com>
2015-10-04 18:42:34 -0300

    fixed minor bug

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -7,11 +7,23 @@ class DataController < ApplicationController
       raise "Value must be specified" if params[:value].blank?
       remote_device = RemoteDevice.find_by_unique_id(request.headers["REMOTE-DEVICE-ID"])
       stream = Stream.where(:remote_device_id => remote_device.id).find_by_unique_id(request.headers["STREAM-ID"])
-      last_datum = StreamData.where(:stream_id => stream.id).order('measured_at asc').last
-      if ((last_datum.measured_at - Time.parse(params[:measured_at]).utc()).abs < 60.seconds ) && ((params[:value].to_i - last_datum.value.to_i).abs > 30)
+      unless StreamData.where(:stream_id => stream.id).blank?
+        last_datum = StreamData.where(:stream_id => stream.id).order('measured_at asc').last
+        if ((last_datum.measured_at - Time.parse(params[:measured_at]).utc()).abs < 60.seconds ) && ((params[:value].to_i - last_datum.value.to_i).abs > 30)
+          response = {
+            :status => "ok"
+          }
+      else
+        stream_data = StreamData.create({:value => params[:value], :stream_id => stream.id, :measured_at => params[:measured_at]}) unless stream.blank?
         response = {
-          :status => "ok"
-        }
+          :status => "ok",
+          :reponse => {
+            :remote_device_name => remote_device.name, 
+            :stream_name => stream.name,
+            :saved_value => stream_data.value
+            }
+          }
+      end
       else
         stream_data = StreamData.create({:value => params[:value], :stream_id => stream.id, :measured_at => params[:measured_at]}) unless stream.blank?
         response = {
