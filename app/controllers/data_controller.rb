fb807923f3159c06b8e73ec3fefaf1c0540a2956
nkipreos <nkipreos@gmail.com>
2015-10-05 10:02:48 -0300

    caching files to make query faster

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -7,12 +7,18 @@ class DataController < ApplicationController
       raise "Value must be specified" if params[:value].blank?
       remote_device = RemoteDevice.find_by_unique_id(request.headers["REMOTE-DEVICE-ID"])
       stream = Stream.where(:remote_device_id => remote_device.id).find_by_unique_id(request.headers["STREAM-ID"])
-      unless StreamData.where(:stream_id => stream.id).blank?
-        last_datum = StreamData.where(:stream_id => stream.id).order('measured_at asc').last
-        if ((last_datum.measured_at - Time.parse(params[:measured_at]).utc()).abs < 60.seconds ) && ((params[:value].to_i - last_datum.value.to_i).abs > 30)
-          response = {
-            :status => "ok"
-          }
+      filename = "tmp/cache/#{stream.id}"
+      if File.exist?(filename)
+        last_datum = File.read(filename)
+      else
+        File.open(filename, 'w') {|x| x.write("{\"measured_at\": \"#{params[:measured_at]}\", \"value\": \"#{params[:value]}\"}")}
+        last_datum = File.read(filename)
+      end
+      last_datum = JSON.parse(last_datum)
+      if ((Time.parse(last_datum['measured_at']).utc() - Time.parse(params[:measured_at]).utc()).abs < 60.seconds ) && ((params[:value].to_i - last_datum['value'].to_i).abs > 30)
+        response = {
+          :status => "ok"
+        }
       else
         stream_data = StreamData.create({:value => params[:value], :stream_id => stream.id, :measured_at => params[:measured_at]}) unless stream.blank?
         response = {
@@ -24,17 +30,7 @@ class DataController < ApplicationController
             }
           }
       end
-      else
-        stream_data = StreamData.create({:value => params[:value], :stream_id => stream.id, :measured_at => params[:measured_at]}) unless stream.blank?
-        response = {
-          :status => "ok",
-          :reponse => {
-            :remote_device_name => remote_device.name, 
-            :stream_name => stream.name,
-            :saved_value => stream_data.value
-            }
-          }
-      end
+      File.open(filename, 'w') {|x| x.write("{\"measured_at\": \"#{params[:measured_at]}\", \"value\": \"#{params[:value]}\"}")}
       render json: response.to_json
     rescue => e
       render_json_exception(e.to_s)
